784d126c2585692def4b54c52349682e1c11f0bf
Carolina Campos <carolinarodriguesjf@gmail.com>
2023-11-03 21:31:44 -0300

    Criação do componente home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './template/nav/nav.component';
 import { FooterComponent } from './template/footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
 
   imports: [
