e4fabc2e571b857ddd346f9094440ddf848f1222
Carolina Campos <carolinarodriguesjf@gmail.com>
2023-11-03 21:22:26 -0300

    Criação dos componentes nav e footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './template/nav/nav.component';
+import { FooterComponent } from './template/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent
   ],
 
   imports: [
