38fe3f31e2b258efcca39ec07ffee158ea96ae5d
Carolina Campos <carolinarodriguesjf@gmail.com>
2023-11-03 21:17:25 -0300

    Configuração do ambiente de desenvolvimento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavComponent } from './template/nav/nav.component';
-import { FooterComponent } from './template/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavComponent,
-    FooterComponent
+    AppComponent
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule,
